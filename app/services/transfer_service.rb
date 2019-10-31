63007d69a69e26c24a13650a3ca034e0672dd18f
Leonardo Lobo Lima <dleemoo@gmail.com>
2019-10-30 22:07:59 -0300

    Adds TransferService
    This service implements the money movement between accounts.
    
    For now, this uses the BalanceService (inside a database lock) to check
    if the source account has sufficient funds to ensure that we don't make
    invalid calculations. Maybe this need to change later.
    
    Only positive values can be used at this operation and the
    implementation ensures that one transaction is created for each account
    and associate then to the same Operation record.
    
    This service need to ensures that the following invariant is always
    respected:
    
      Operation.joins(:debit, :credit).sum("debits.amount + credits.amount")

---
+++ app/services/transfer_service.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+class TransferService
+  include CallInterface
+  InsufficientFunds = Class.new(RuntimeError)
+
+  Schema = Dry::Schema.Params do
+    required(:source_account_id).filled(Types::UUID)
+    required(:target_account_id).filled(Types::UUID)
+    required(:amount).filled(Types::Params::Decimal) { gt?(0) }
+  end
+
+  def call(input)
+    params = yield Schema.call(input)
+    source = yield fetch_account(params, :source_account_id)
+    target = yield fetch_account(params, :target_account_id)
+
+    Success(transfer!(source, target, params[:amount]))
+  rescue InsufficientFunds
+    Error(source_account_id: ["insufficient funds"])
+  end
+
+  private
+
+  def fetch_account(params, key)
+    Maybe(Account.find_by_id(params[key]))
+      .or(Error(key => ["not found"]))
+  end
+
+  def transfer!(source, target, amount)
+    source.with_lock do
+      balance = BalanceService.call(account_id: source.id)
+      raise InsufficientFunds if balance.value! < amount
+
+      Operation.create!(kind: "transfer").tap do |operation|
+        Debit.create!(account: source, operation: operation, amount: -amount)
+        Credit.create!(account: target, operation: operation, amount: amount)
+      end
+    end
+  end
+end
---
+++ spec/services/transfer_service_spec.rb
@@ -0,0 +1,99 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe TransferService do
+  subject(:service) { described_class }
+
+  let(:deposit) { DepositService }
+  let(:balance) { BalanceService }
+
+  let(:source_account) { Account.create!(name: "source account") }
+  let(:target_account) { Account.create!(name: "target account") }
+
+  context "with valid parameters" do
+    it "makes the transfer between the accounts" do
+      deposit.call(account_id: source_account.id, amount: 100)
+
+      result = nil
+
+      expect { result = service.call(source_account_id: source_account.id, target_account_id: target_account.id, amount: 10) }
+        .to change(Debit.where(account_id: source_account), :count).by(1)
+        .and change(Credit.where(account_id: target_account), :count).by(1)
+
+      expect(result).to be_success
+
+      operation = result.value!
+
+      expect(operation.kind).to eq("transfer")
+
+      expect(operation.credit.amount).to eq(10)
+      expect(operation.target_account).to eq(target_account)
+
+      expect(operation.debit.amount).to eq(-10)
+      expect(operation.source_account).to eq(source_account)
+
+      expect(balance.call(account_id: source_account.id).value!).to eq(90)
+      expect(balance.call(account_id: target_account.id).value!).to eq(10)
+    end
+  end
+
+  context "with insuficient funds in source account" do
+    it "denies the transaction" do
+      deposit.call(account_id: source_account.id, amount: 100)
+
+      result = nil
+
+      expect { result = service.call(source_account_id: source_account.id, target_account_id: target_account.id, amount: 100.01) }
+        .to change(Debit.where(account_id: source_account), :count).by(0)
+        .and change(Credit.where(account_id: target_account), :count).by(0)
+
+      expect(result).to be_failure
+      expect(result.failure.errors.to_h).to eq(source_account_id: ["insufficient funds"])
+
+      expect(balance.call(account_id: source_account.id).value!).to eq(100)
+      expect(balance.call(account_id: target_account.id).value!).to eq(0)
+    end
+  end
+
+  context "with invalid paramaters" do
+    it "does not allows negative transfers" do
+      result = nil
+
+      expect { result = service.call(source_account_id: source_account.id, target_account_id: target_account.id, amount: -10) }
+        .to change(Transaction, :count).by(0)
+        .and change(Operation, :count).by(0)
+
+      expect(result).to be_failure
+      expect(result.failure.errors.to_h).to eq(amount: ["must be greater than 0"])
+    end
+
+    it "denies transfers to inexistent source account" do
+      result = service.call(source_account_id: SecureRandom.uuid, target_account_id: target_account.id, amount: 10)
+
+      expect(result).to be_failure
+      expect(result.failure.errors.to_h).to eq(source_account_id: ["not found"])
+    end
+
+    it "denies transfers to inexistent target account" do
+      result = service.call(source_account_id: source_account.id, target_account_id: SecureRandom.uuid, amount: 10)
+
+      expect(result).to be_failure
+      expect(result.failure.errors.to_h).to eq(target_account_id: ["not found"])
+    end
+
+    it "denies transfers to invalid source account" do
+      result = service.call(source_account_id: "abc", target_account_id: target_account.id, amount: 10)
+
+      expect(result).to be_failure
+      expect(result.failure.errors.to_h).to eq(source_account_id: ["is in invalid format"])
+    end
+
+    it "denies transfers to invalid target account" do
+      result = service.call(source_account_id: source_account.id, target_account_id: "abc", amount: 10)
+
+      expect(result).to be_failure
+      expect(result.failure.errors.to_h).to eq(target_account_id: ["is in invalid format"])
+    end
+  end
+end
