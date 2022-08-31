1c2279d13d083967d2fc242b400293cf96f8d64e
Leonardo Lobo Lima <dleemoo@gmail.com>
2022-08-31 18:54:46 -0300

    Remove unused code
    With services removal some code is not used and can be removed.

--- app/types/types.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Types
-  include Dry.Types()
-
-  UUID = String.constrained(format: /^\h{8}(-\h{4}){3}-\h{12}$/)
-end
--- config/initializers/dry.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require "dry/schema"
-require "dry/monads"
-require "dry/monads/do"
-require "dry/types"
-require "dry/struct"
-
-Dry::Schema.load_extensions(:monads)
